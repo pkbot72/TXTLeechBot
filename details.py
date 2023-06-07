9b1dc8e9411f3300ab283ec62a47733234e610a4
pkbot72 <101813111+pkbot72@users.noreply.github.com>
2023-06-07 12:29:58 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 26435700
-api_hash = "527cf5174e120a9093611bc69d7b7709"
-bot_token = "6180767554:AAGKw-Eo6L0DXlL4BsrDmKItUewgdKGAtGo"
+api_id = 27584273
+api_hash = "1fcbb51a52ae000357b189482cdd7179"
+bot_token = "6161104179:AAHuySaB8zWp0lSjZJr5SCMa5t8QETN5ux0"
 
